51446c9733ec740bd69eebcf425a76d2d5ed896a
mungchan <85596908+mungchan20@users.noreply.github.com>
2026-09-09 14:26:46 +0900

    파일 저장 추가

--- budjet.py
+++ budjet.py
@@ -8,8 +8,10 @@ class BudgetTracker:
 
     # 지출 목록보기
     def show_expenses(self):
+        num = 1
         for expense in self.expenses:
-            print(f"{expense['제목']}- {expense['가격']}원 ({expense['유형']})")
+            print(f"{num}.{expense['제목']}- {expense['가격']}원 ({expense['유형']})")
+            num+=1
 
     # 지출 총액
     def total_expense(self):
@@ -18,18 +20,34 @@ class BudgetTracker:
             total += expense["가격"]
         return total 
 
+    # 지출 유형별 합계
+    def total_by_type(self,type):
+        total = 0
+        for expense in self.expenses:
+            if expense["유형"] == type:
+                total += expense["가격"]
+        return total       
+
+    # 지출 삭제하기
+    def delete_expense(self,num):
+        del self.expenses[num-1]
+
 print("가계부에 오신 것을 환영합니다.")
 tracker = BudgetTracker()
+valid_type = ["카드","현금"]
 
 while True:
-    print("1.지출 추가하기\n2.지출 목록보기\n3.총 지출 확인하기\n4.종료하기")
+    print("1.지출 추가하기\n2.지출 목록보기\n3.총 지출 확인하기\n4.지출 삭제하기\n5.유형별 합계\n6.종료하기")
     num = int(input("원하시는 번호를 입력해주세요: "))
     if num == 1:    
         # 지출  추가하기
         title = input("지출 제목: ")
         price = int(input("가격 : "))
         type = input("지출 유형(카드/현금): ")
-        tracker.add_expense(title,price,type)
+        if type in valid_type:
+            tracker.add_expense(title,price,type)
+        else:
+            print("카드나 현금중에서만 입력해라")
     elif num == 2:    
         # 지출 목록보기
         tracker.show_expenses()
@@ -37,6 +55,18 @@ while True:
         # 총 지출 확인하기
         print(f"총 지출 : {tracker.total_expense()}")
     elif num == 4:
+        del_num = int(input("삭제하실 지출의 번호를 입력: "))
+        if 1<=del_num<=len(tracker.expenses):
+            tracker.delete_expense(del_num)
+        else:
+            print("똑바로 써라")
+    elif num == 5:
+        type = input("유형을 골라주세요:")
+        if type in valid_type:
+            print(f"{type} 합계 : {tracker.total_by_type(type)}")
+        else:
+            print("카드 또는 현금만 입력 가능")
+    elif num == 6:
         break
     else:
         print("똑바로 써라")
